3f547264bfe0c0e7f52926b945bb60d5d56e8ac1
Your Name <you@example.com>
2023-10-07 20:03:18 -0700

    added Git class

--- src/test/java/GitClass.java
+++ src/test/java/GitClass.java
@@ -3,12 +3,14 @@ public class GitClass {
     ////leaving comment
 
     ///comment #2
-   ///message #3
+   ///message
 
     /// new
 
   /// one more comment
       // // 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
+
+    ///  message #3
 }
 
 
